839faecfbe23f2bade56ce40ce40707c9e316ab8
Dmitry <daver00@mail.ru>
2023-02-25 14:49:47 +0300

    Добавьте файлы проекта.

---
+++ 1/Header.h
@@ -0,0 +1,5 @@
+#pragma once
+int hod(int v, int* a1, string* s11, string* s12, string* s13, string* s21, string* s22, string* s23, string* s31, string* s32, string* s33,
+	string* A11, string* A12, string* A13, string* A21, string* A22, string* A23, string* A31, string* A32, string* A33);
+int inter(string s11, string s12, string s13, string s21, string s22, string s23, string s31, string s32, string s33,
+	string A11, string A12, string A13, string A21, string A22, string A23, string A31, string A32, string A33, string imz1, string imz2, int a);
